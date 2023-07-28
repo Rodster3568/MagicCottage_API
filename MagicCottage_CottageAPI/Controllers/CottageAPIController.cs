1ccfa6ec7df940a6545379eac1e649c92d156c3a
Kopliness <mr.vladlis3568@gmail.com>
2023-07-28 12:08:09 +0300

    add delete method

--- MagicCottage_CottageAPI/Controllers/CottageAPIController.cs
+++ MagicCottage_CottageAPI/Controllers/CottageAPIController.cs
@@ -16,7 +16,7 @@ namespace MagicCottage_CottageAPI.Controllers
         {
             return Ok(CottageStore.cottageList);
         }
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="GetCottage")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -34,22 +34,45 @@ namespace MagicCottage_CottageAPI.Controllers
             return Ok(cottage);
         }
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<CottageDTO> CreateCottage([FromBody]CottageDTO cottageDTO)
+        public ActionResult<CottageDTO> CreateCottage([FromBody] CottageDTO cottageDTO)
         {
-            if(cottageDTO == null)
+            if (CottageStore.cottageList.FirstOrDefault(u => u.Name.ToLower() == cottageDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Cottage already exists");
+                return BadRequest(ModelState);
+            }
+            if (cottageDTO == null)
             {
                 return BadRequest(cottageDTO);
             }
-            if(cottageDTO.Id > 0)
+            if (cottageDTO.Id > 0)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
-            cottageDTO.Id=CottageStore.cottageList.OrderByDescending(u=>u.Id).FirstOrDefault().Id+1;
+            cottageDTO.Id = CottageStore.cottageList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
             CottageStore.cottageList.Add(cottageDTO);
-            return Ok(cottageDTO);
+            return CreatedAtRoute("GetCottage", new { id = cottageDTO.Id }, cottageDTO);
+        }
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpDelete("{id:int}", Name = "DeleteCottage")]
+        public IActionResult DeleteCottage(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var cottage = CottageStore.cottageList.FirstOrDefault(u => u.Id == id);
+            if (cottage == null)
+            {
+                return BadRequest();
+            }
+            CottageStore.cottageList.Remove(cottage);
+            return NoContent();
         }
     }
 }
--- MagicCottage_CottageAPI/Models/Dto/CottageDTO.cs
+++ MagicCottage_CottageAPI/Models/Dto/CottageDTO.cs
@@ -1,8 +1,14 @@
-﻿namespace MagicCottage_CottageAPI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicCottage_CottageAPI.Models.Dto
 {
     public class CottageDTO
     {
+        
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [MinLength(5)]
         public string Name { get; set; }
     }
 }
